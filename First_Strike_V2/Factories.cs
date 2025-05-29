58fbc3f96b3fb114ad9899884eba3cb145bfb003
Or Zellinger <or.zellinger@gmail.com>
2025-05-29 14:47:10 +0300

    Complete Terrorist Factory

---
+++ First_Strike_V2/Factories.cs
@@ -0,0 +1,14 @@
+﻿namespace First_Strike_V2.Models
+{
+    public class TerroristsFactory
+    {
+        public List<Terrorist> ListOfTerroristFactory;
+
+        public Terrorist CreateTerrorist(string name, int rank)
+        {
+            Terrorist trr = new Terrorist(name, rank);
+            ListOfTerroristFactory.Add(trr);
+            return trr;
+        }
+    }
+}
