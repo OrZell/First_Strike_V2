f2822327632d35cbeef3d745250fc31582b1903b
Or Zellinger <or.zellinger@gmail.com>
2025-05-29 16:24:55 +0300

    Created the TerroristFactory class

---
+++ First_Strike_V2/TerroristFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace First_Strike_V2.Models
+{
+    public class TerroristFactory
+    {
+        public class TerroristsFactory
+        {
+            public List<Terrorist> ListOfTerroristByFactory;
+
+            public Terrorist CreateTerrorist(string name, int rank)
+            {
+                Terrorist trr = new Terrorist(name, rank);
+                ListOfTerroristByFactory.Add(trr);
+                return trr;
+            }
+        }
+    }
+}
