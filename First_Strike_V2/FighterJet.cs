0fa1e865cc515e33e6648043ef02fd8376fdd6c5
Daniel Fisher <danielg16fisher@gmail.com>
2025-05-29 11:13:15 +0300

    Fighter Jet F-16 framework

---
+++ First_Strike_V2/FighterJet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace First_Strike_V2
+{
+    public class FighterJet : StrikeOptions
+    {
+        public List<string> BombType;
+        public string Operator;
+
+        public FighterJet() : base()
+        {
+            Name = "F-16";
+            AmmunitionCapacity = 8;
+            Fuel = 100;
+            EffectiveAgainst = "buildings";
+            BombType = { "0.5 Ton", "1 Ton"}
+            
+        }
+    }
+}
+
