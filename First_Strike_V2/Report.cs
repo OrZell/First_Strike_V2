5526a63be13363155eedbb5d4105b2a09681a6ea
Or Zellinger <or.zellinger@gmail.com>
2025-05-29 15:25:53 +0300

    Complete Report class

---
+++ First_Strike_V2/Report.cs
@@ -0,0 +1,19 @@
+﻿using First_Strike_V2.Models;
+using Microsoft.VisualBasic;
+
+namespace First_Strike_V2
+{
+    public class Report
+    {
+        public Terrorist TerroristPerson;
+        public string Place;
+        public string DateAndTime;
+
+        public Report(Terrorist terrorist, string place, string dateandtime)
+        {
+            this.TerroristPerson = terrorist;
+            this.Place = place;
+            this.DateAndTime = dateandtime;
+        }
+    }
+}
