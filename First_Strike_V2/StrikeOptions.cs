bc17de368ec07f863c52aa8d9d2e7eed91a3c419
Daniel Fisher <danielg16fisher@gmail.com>
2025-05-29 11:03:53 +0300

    Strike Options framework

---
+++ First_Strike_V2/StrikeOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace First_Strike_V2
+{
+    public class StrikeOptions
+    {
+        public string Name;
+        public int AmmunitionCapacity;
+        public float Fuel;
+        public string EffectiveAgainst;
+
+        public StrikeOptions(string name, int ammunitionCapacity, float fuel, string effectiveAgainst)
+        {
+            Name = name;
+            AmmunitionCapacity = ammunitionCapacity;
+            Fuel = fuel;
+            EffectiveAgainst = effectiveAgainst;
+        }
+    }
+}
+
