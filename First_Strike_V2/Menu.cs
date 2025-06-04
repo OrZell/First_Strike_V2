6f2acd7d155a88b285af01f93cab03fbf4a74469
Or Zellinger <or.zellinger@gmail.com>
2025-06-04 20:39:57 +0300

    Created the first menu

---
+++ First_Strike_V2/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+
+namespace First_Strike_V2.Models
+{
+    public class Menu
+    {
+        public Aman? aman;
+        public IDF? idf;
+
+        public Menu(Aman aman, IDF idf)
+        {
+            this.aman = aman;
+            this.idf = idf;
+        }
+    }
+}
